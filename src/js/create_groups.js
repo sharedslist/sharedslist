d3c221e52f0a8e928fbe182b96652cd054f00e5d
ARecuenco92 <alvaro.recuenco92@gmail.com>
2013-10-28 23:53:41 +0100

    Control de errores
    Olvidé subirlo antes

--- src/js/create_groups.js
+++ src/js/create_groups.js
@@ -4,11 +4,31 @@ function addUsers()
 {
 	var txt = $("#text-2");
 	var val = txt.val();
-	if(val != ""){
+	if(validateEmail(val)){
 		$("#check2").append('<input type="checkbox" checked id="cb'+id+'"/><label id="cb'+id+'"for="cb'+id+'">'+val+'</label>');
 		$("#check").trigger("create");
 		id = id +1;
 		$("#text-2").val("");
+		$('#message').html('');
+	}
+	else{
+		$('#message').html('Email incorrecto');
+	}
+}
+
+function validateEmail(email) 
+{ 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+} 
+
+function validateName(name) 
+{ 	
+	if($.trim(name) == ""){
+		return false;
+	}
+	else{
+		return true;
 	}
 }
 
@@ -16,20 +36,25 @@ function createGroup()
 {
 	var users = new Array();
 	var group_name = $("#text-1").val();
-	var n = $('form#createform').find('input:checked');
-	var name;
-	for(var i=1; i< n.length+1; i++)
-	{	
-		name = $("#"+n[i-1].id).next("label").text();
-		users[i-1] = {'name' : $.trim(name)};
+	if(validateName(group_name)){
+		var n = $('form#createform').find('input:checked');
+		var name;
+		for(var i=1; i< n.length+1; i++)
+		{	
+			name = $("#"+n[i-1].id).next("label").text();
+			users[i-1] = {'name' : $.trim(name)};
+		}
+		var parameters = { "users" : users, "group_name" : group_name};
+		$.ajax({
+			data:  parameters,
+			url:   document.URL+'/../php/create_groups.php',
+			dataType: 'text',
+			type:  'post',
+			success:  function (response)
+				   {window.location.href = 'list_groups.html';}
+			});
 	}
-	var parameters = { "users" : users, "group_name" : group_name};
-	$.ajax({
-		data:  parameters,
-		url:   document.URL+'/../php/create_groups.php',
-		dataType: 'text',
-		type:  'post',
-		success:  function (response)
-			   {window.location.href = 'list_groups.html';}
-		});
+	else{
+		$('#message').html('Nombre de grupo vacio');
+	}
 }
