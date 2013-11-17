2ae963cf2823725850c9472c455cfdf5e213404a
ARecuenco92 <alvaro.recuenco92@gmail.com>
2013-11-17 17:31:06 +0100

    Acceso a la listas

--- src/php/list_groups.php
+++ src/php/list_groups.php
@@ -6,6 +6,6 @@
 	$group = new Group;							// Crean un [Grupo]
 	$user = new User;							// Crea un [Usuario]
 	$user_id = $user->getLoggedInUser()->id; 	// Obtiene el identificador del usuario en sesión
-	$names = $group->listGroups($user_id); 		// Devuelve una lista con los nombres de los grupos a los que pertenece $user_id
-	echo '{"names":'. json_encode($names) .'}'; // Empaqueta la respuesta
+	$groups = $group->listGroups($user_id); 		// Devuelve una lista con los nombres de los grupos a los que pertenece $user_id
+	echo json_encode($groups);  // Empaqueta la respuesta
 	?>
