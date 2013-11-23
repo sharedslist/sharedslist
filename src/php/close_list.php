e4a3c4348c9819b5d247ed9f41623b5a5e3f9996
marlavi <laliena.marta@gmail.com>
2013-11-24 00:31:05 +0100

    Created close_list.php

---
+++ src/php/close_list.php
@@ -0,0 +1,27 @@
+﻿<?php
+	require_once("User.php");
+	require_once("ShoppingList.php");
+	
+	session_start();
+	if( isset($_SESSION['idList']) ) {
+		$idList = $_SESSION['idList']; //obtenemos el id de la lista a partir de la sesión
+	}
+	else if ( isset($_POST['idList']) ) {
+		$idList = $_POST['idList']; //obtenemos el id de la lista a partir de la variable POST
+	}
+	else {
+		//die ('No se ha seleccionado una lista');
+	}
+	
+	//comprobamos que el usuario se ha autenticado y pertenece al grupo cuya lista quiere listar
+	$currentUser = User::getLoggedInUser();
+	if( !$currentUser ) {
+		die ('Necesitas autenticarte para acceder a esta funcionalidad');
+	}
+	if( !ShoppingList::userBelongsToGroupOfList($currentUser->id,$idList) ) {
+		die ("No perteneces al grupo de la lista con id $idList!");
+	}
+	
+	// cerramos la lista con identificador idList
+	ShoppingList::closeList($idList);
+?>
