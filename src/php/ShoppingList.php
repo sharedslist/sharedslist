796804bfc2a39909d14e027ec72d12abd175b0d8
lukasged <lukas_ged@hotmail.com>
2013-11-23 12:08:30 +0100

    Corregido ShoppingList.php
    Se han recuperado los comentarios en castellano que se habían perdido
    por algun error.
    Además, se ha eliminado la parte del código donde se recuperaban los
    items de cada lista de compra ya que ahora no se muestran en el listado
    de las listas de compra.

--- src/php/ShoppingList.php
+++ src/php/ShoppingList.php
@@ -4,44 +4,40 @@ require_once("config.php");
 require_once("Item.php");
 
 /**
-  * Class to handle app shopping lists
+  * Clase para manejar las listas de compras
   */
 class ShoppingList
 {
 	/**
-      * @var int The list ID from the database
+      * @var int El ID de la lista en la base de datos
       */
 	public $idList = null;
   
 	/**
-      * @var int The group ID from the database
+      * @var int El ID del grupo en la base de datos
       */
 	public $idGroup = null;
   
 	/**
-      * @var string The list's name
+      * @var string El nombre de la lista
       */
 	public $listName = null;
 	
 	/**
-      * @var boolean The list's state
+      * @var boolean El estado de la lista
       */
 	public $listState = null;
 	
 	/**
-      * @var string The list's creation's timestamp
+      * @var string El timestampo de la creación de la lista
       */
 	public $listCreated = null;
 	
-	/**
-      * @var string The list's creation's timestamp
-      */
-	public $items = null;
 	
 	/**
-      * Sets the object's properties using the values in the supplied array
+	  * Establece las propiedades del objeto usando los valores en el array proporcionado
       *
-      * @param assoc The property values
+      * @param assoc Los valores de las propiedades
       */
 	public function __construct( $data=array() ) {
 		if ( isset( $data['idList'] ) ) $this->idList = (int) $data['idList'];
@@ -49,18 +45,18 @@ class ShoppingList
 		if ( isset( $data['listName'] ) ) $this->listName = $data['listName'];
 		if ( isset( $data['listState'] ) ) $this->listState = $data['listState'];
 		if ( isset( $data['listCreated'] ) ) $this->listCreated = $data['listCreated'];
-		if ( isset( $data['items'] ) ) $this->items = $data['items'];
 	}	
   
 	/**
-      * Inserts the ShoppingList object into the database, and sets its ID property.
+	  * Inserta la lista de compras en la base de datos y establece su ID proporcionado
+	  * por la base de datos.
       */
 	public function insert() {
 
 		$con = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DBNAME);
 		
 		if (!$con) {
-			die('Could not connect: ' . mysqli_error($con));
+			die('No se ha podido conectar: ' . mysqli_error($con));
 		}
 	
 		$sql = "INSERT INTO `ShoppingList` (idGroup, listName) values ('".$this->idGroup."', '".$this->listName."')";
@@ -70,29 +66,25 @@ class ShoppingList
 	}
   
 	/**
-	  * Obtains all the shopping lists and their items of the group specified by 'group'
+	  * Obtiene todas las listas de compras del grupo
+	  * especificado por el identificador del grupo 'group'
+	  *
+	  * @param int $group El ID del grupo cuyas listas de compras se quieren listar
 	  */
 	public static function listSLists($group) {
   
 		$con = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DBNAME);
     
 		if (!$con) {
-			die('Could not connect: ' . mysqli_error($con));
+			die('No se ha podido conectar: ' . mysqli_error($con));
 		}
 		//primero obtenemos las listas del grupo 'group'
 		$sql = "SELECT * FROM `ShoppingList` WHERE idGroup = $group";
 		$result = mysqli_query($con, $sql);
 		$lists = [];
+		//iteramos sobre las listas y las añadimos al vector de listas
 		while ($row=mysqli_fetch_array($result)) {
 			$slist = new ShoppingList($row);
-			//obtenemos los items correspondientes a cada lista
-			$sql_items = "SELECT * FROM `Item` WHERE idList = ".$row['idList'];
-			$result_items = mysqli_query($con, $sql_items);
-			$items = [];
-			while ($row_item=mysqli_fetch_array($result_items)) {
-				$items[] = new Item($row_item);
-			}
-			$slist->items = $items;
 			$lists[] = $slist;
 		}
 		
