28e1c9662e90f324509886f17f77ea7d1f4cd172
marlavi <laliena.marta@gmail.com>
2013-11-24 11:29:09 +0100

    Updated list_items.php

--- src/php/list_items.php
+++ src/php/list_items.php
@@ -1,28 +1,31 @@
 ﻿<?php
 	require_once("Item.php");
-	//require_once("User.php");
-	require_once("config.php");
+	require_once("User.php");
+	require_once("ShoppingList.php");
 	
 	session_start();
-	$idList = $_POST['idList'] //obtenemos el id del grupo a partir de la sesión
-	//$idList = 15 ; //lo pongo a 10 mientras no pueda obtener el idList de la sesión
-	
-	$con = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DBNAME);
-	if (!$con) {
-		die('Could not connect: ' . mysqli_error($con));
+	if( isset($_SESSION['idList']) ) {
+		$idList = $_SESSION['idList']; //obtenemos el id de la lista a partir de la sesión
+	}
+	else if ( isset($_POST['idList']) ) {
+		$idList = $_POST['idList']; //obtenemos el id de la lista a partir de la variable POST
+	}
+	else {
+		//die ('No se ha seleccionado una lista');
 	}
 	
-	//obtenemos el nombre del grupo actualmente seleccionado
-	$sql = "SELECT * FROM `ShoppingList` WHERE idList = ".$idList;
-	$result = mysqli_query($con, $sql);
-	$row = mysqli_fetch_array($result);
-	$listName = $row['listName'];
-	
-	
-	//obtenemos las listas de compra del grupo actualmente seleccionado
-	$items = Item::listItems($idList);
-	//juntamos el nombre del grupo y sus listas de compra en un array
-	$listAndItems = array( 'listName' => $listName, 'items' => $items);
-	//lo codificamos en JSON y lo enviamos al usuario
-	echo json_encode($listAndItems); 
+	//comprobamos que el usuario se ha autenticado y pertenece al grupo cuya lista quiere listar
+	$currentUser = User::getLoggedInUser();
+	if( !$currentUser ) {
+		die ('Necesitas autenticarte para acceder a esta funcionalidad');
+	}
+	if( !ShoppingList::userBelongsToGroupOfList($currentUser->id,$idList) ) {
+		die ("No perteneces al grupo de la lista con id $idList!");
+	}
+
+	// obtenemos el nombre de la lista (listName) y sus items (items)
+	$listAndItems = listItems($idList);
+	// lo codificamos en JSON y lo enviamos al usuario
+	mysqli_close($con);
+	echo json_encode($listAndItems);
 	?>
