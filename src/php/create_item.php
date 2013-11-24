e0a634659283629ce005c3472de96f43c66be0d4
marlavi <laliena.marta@gmail.com>
2013-11-24 10:35:51 +0100

    Quitar obtención parámetros de _SESSION

--- src/php/create_item.php
+++ src/php/create_item.php
@@ -1,13 +1,39 @@
 ﻿<?php
 	require_once("Item.php");
+	require_once("User.php");
+	require_once("ShoppingList.php");
 
-	$itemName = $_POST["itemName"];
-	$quantity = $_POST["quantity"];
-	
 	session_start();
+	if( isset($_POST['idList']) ) {
+		$idList = $_POST["idList"]; //obtenemos el id del item a partir de la variable POST
+	}
+	else {
+		//die ('No se ha seleccionado un item');
+	}
+	if( isset($_POST["itemName"]) ) {
+		$itemName = $_POST["itemName"]; //obtenemos el nombre del item a partir de la variable POST
+	}
+	else {
+		//die ('No se ha seleccionado un item');
+	}
+	if( isset($_POST['quantity']) ) {
+		$quantity = $_POST["quantity"]; //obtenemos la cantidad del item a partir de la variable POST
+	}
+	else {
+		//die ('No se ha seleccionado un item');
+	}
+	
+	//comprobamos que el usuario se ha autenticado y pertenece al grupo en cuya lista se encuentra el item eliminar
+	$currentUser = User::getLoggedInUser();
+	if( !$currentUser ) {
+		die ('Necesitas autenticarte para acceder a esta funcionalidad');
+	}
+	if( !ShoppingList::userBelongsToGroupOfList($currentUser->id,$idList) ) {
+		die ("No perteneces al grupo de la lista con id $idList!");
+	}
+	
 	$item = new Item;
-	//$item->idList = getListId();
-	$item->idList = 1;
+	$item->idList = $idList;
 	$item->itemName = $itemName;
 	$item->itemState = false;
 	$item->quantity = $quantity;
--- src/php/delete_item.php
+++ src/php/delete_item.php
@@ -1,9 +1,25 @@
 ﻿<?php
 	require_once("Item.php");
+	require_once("User.php");
+	require_once("ShoppingList.php");
 	
-	$idItem = $_POST["idItem"];
+	session_start();
+	if( isset($_POST['idItem']) ) {
+		$idItem = $_POST["idItem"]; //obtenemos el id del item a partir de la variable POST
+	}
+	else {
+		//die ('No se ha seleccionado un item');
+	}
 	
-	$item = new Item;
-	$item->idItem = $idItem;
+	//comprobamos que el usuario se ha autenticado y pertenece al grupo en cuya lista se encuentra el item eliminar
+	$currentUser = User::getLoggedInUser();
+	if( !$currentUser ) {
+		die ('Necesitas autenticarte para acceder a esta funcionalidad');
+	}
+	$item = Item::getItemById($idItem);
+	if( !ShoppingList::userBelongsToGroupOfList($currentUser->id,$item->idList) ) {
+		die ("No perteneces al grupo de la lista con id $idList!");
+	}
+
 	$item->deleteItem();
 ?>
