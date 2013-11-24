145caf16133ee685ab986e929e7651c3b080636f
lukasged <lukas_ged@hotmail.com>
2013-11-24 16:11:53 +0100

    Se ha añadido la funcionalidad de borrar listas
    Se ha modificado la GUI de list_slists : ahora al mantener una pulsación
    larga sobre una lista , se muestra un menú contextual. También se
    muestra el mismo menú al pulsar el icono que aparece a la derecha de
    cada lista. Dicho menú permite ver y editar una lista , cerrarla o
    borrarla. Cerrar o borrar lista solicita confirmación.
    Además,  al simplemente pulsar sobre una lista se redirige a la página
    en la que se muestran los items de dicha lista.

---
+++ src/php/delete_list.php
@@ -0,0 +1,26 @@
+﻿<?php
+	require_once("User.php");
+	require_once("ShoppingList.php");
+	
+	if ( isset($_POST['idList']) ) {
+		$idList = $_POST['idList']; //obtenemos el id de la lista a partir de la variable POST
+	}
+	else {
+		//die ('No se ha seleccionado una lista');
+	}
+	
+	//comprobamos que el usuario se ha autenticado y pertenece al grupo cuya lista quiere eliminar
+	session_start();
+	$currentUser = User::getLoggedInUser();
+	if( !$currentUser ) {
+		die ('Necesitas autenticarte para acceder a esta funcionalidad');
+	}
+	if( !ShoppingList::userBelongsToGroupOfList($currentUser->id,$idList) ) {
+		die ("No perteneces al grupo de la lista con id $idList!");
+	}
+	
+	// eliminamos la lista con identificador idList
+	ShoppingList::deleteList($idList);
+	//indicamos que la operación ha finalizado con éxito
+	echo 'success';
+?>
