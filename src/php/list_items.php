66d4106e7452645cf5e3213bedb1cd206b274917
marlavi <laliena.marta@gmail.com>
2013-11-25 21:52:47 +0100

    Update list_items.php

--- src/php/list_items.php
+++ src/php/list_items.php
@@ -1,31 +1,26 @@
-﻿<?php
+<?php
 	require_once("Item.php");
 	require_once("User.php");
 	require_once("ShoppingList.php");
 	
 	session_start();
-	if( isset($_SESSION['idList']) ) {
-		$idList = $_SESSION['idList']; //obtenemos el id de la lista a partir de la sesión
-	}
-	else if ( isset($_POST['idList']) ) {
+	if ( isset($_POST['idList']) ) {
 		$idList = $_POST['idList']; //obtenemos el id de la lista a partir de la variable POST
 	}
 	else {
-		//die ('No se ha seleccionado una lista');
+		die ('No se ha seleccionado una lista');
 	}
-	
 	//comprobamos que el usuario se ha autenticado y pertenece al grupo cuya lista quiere listar
 	$currentUser = User::getLoggedInUser();
 	if( !$currentUser ) {
 		die ('Necesitas autenticarte para acceder a esta funcionalidad');
 	}
 	if( !ShoppingList::userBelongsToGroupOfList($currentUser->id,$idList) ) {
-		die ("No perteneces al grupo de la lista con id $idList!");
+		die ("No perteneces al grupo de la lista cuyos productos quieres listar");
 	}
 
 	// obtenemos el nombre de la lista (listName) y sus items (items)
-	$listAndItems = listItems($idList);
+	$listAndItems = Item::listItems($idList);
 	// lo codificamos en JSON y lo enviamos al usuario
-	mysqli_close($con);
 	echo json_encode($listAndItems);
-	?>
+?>
