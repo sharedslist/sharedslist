9d54327462f117b4016b9bc26639b0d9776321a4
cbarzu <627625@unizar.es>
2013-11-30 20:21:04 +0100

    tareas de claudiu a falta de pruebas.

--- src/js/list_members.js
+++ src/js/list_members.js
@@ -1,3 +1,4 @@
+var admin = false;
 
 function listMembers() {
 	$.ajax({
@@ -5,8 +6,14 @@ function listMembers() {
 		dataType: 'text',
 		type:  'post',
 		success:  function (response) {
-					var obj1 = JSON.parse(response.trim());
-					listarMiembros(obj1.miembros);
+					try{
+						var obj1 = JSON.parse(response.trim());
+						admin=obj1.admin;
+						listarMiembros(obj1);
+					}catch (err){
+						$("#message").html("");
+						$("#message").html("Ha ocurrido un error. Pruebe de nuevo.");
+					}
 				}
 		});
 }
@@ -14,13 +21,66 @@ function listMembers() {
 /*
  * Lista los nombres de los miembros del grupo
  */
-function listarMiembros(miembros)
+function listarMiembros(response)
 {
 	$("#miembros").html("");
-	for(var i=0; i< miembros.length; i++)
+	for(var i=0; i< response.miembros.length; i++)
 	{
-		$("#miembros").append('<li >'+miembros[i]+'</li>');
+		var divider = '<li data-theme = "d" idMember="' + response.ids[i] + '"> ';
+		var member = '<a href="#">'+ response.miembros[i] + '</a>';
+		if (admin){
+			var options = '<a href ="#" class="btnBorrar" idMember="'+response.ids[i]+'">BorrarMiembro</a>';
+			divider = divider + member + options + '</li>';
+		}else {
+			divider = divider + member + '</li>';
+		}
+		
+		$("#miembros").append(divider);
+		//$("#miembros").append('<li id="'+ response.ids[i]+'"><a href ="#">'+response.miembros[i]+'</a></li>');
 		$("#miembros").listview('refresh');
 	}
 	$("#miembros").trigger("create");
 }
+
+
+$(document).on('click', '.btnBorrar', function(boton) {
+	var idMember = boton.currentTarget.attributes.idmember.value;
+	$('#popupConfirmDeleteMember').attr("idMember", idMember);
+	//mostramos un mensaje informando de la operación a realizar
+	$("#txtConfirm").html("¿Seguro que quiere expulsar al miembro del grupo?");
+	//cerramos el popup de las opciones
+	$('#popupBasic').popup("close");
+	//mostramos el popup de la confirmación
+	setTimeout( function(){ $('#popupConfirmDeleteMember').popup( 'open', { transition: "pop" } ) }, 100 );
+});
+
+/*
+ * Asociamos el evento 'click' al botón de confirmación del abandono del grupo
+ */
+$(document).on('click', '.btnConfirmDelete', function() {
+	//obtenemos el ID del grupo que se quiere abandonar
+	var idMember = $('#popupConfirmDeleteMember').attr("idMember");
+
+	//abandonar grupo
+	$.ajax({
+		url: 'php/delete_member.php',
+		dataType: 'text',
+		data: {"idMember" : idMember},
+		type:  'post',
+		success:  function (response)
+				{
+					var status = response.trim();
+					if(status == 'success') {
+						//refrescamos la página
+						location.reload();
+					} else {
+						$('#message').html(status);
+					}
+				},
+		error: 	function() {
+					$("#message").html("Ha ocurrido un error intentando expulsar al usuario");
+				}
+	});
+	//cerramos el popup de la confirmación
+	$('#popupConfirmDeleteMember').popup('close');
+});
